963d03b7259ddb9d983bb6832e498aa0df04eb8f
Rachit Mohan Garg <rachit.mohan.garg@accenture.com>
2016-01-20 12:09:56 +0000

    CarpoolerDao

--- src/main/java/com/carpool/dao/Carpooler.java
+++ src/main/java/com/carpool/dao/Carpooler.java
@@ -2,4 +2,76 @@ package com.carpool.dao;
 
 public class Carpooler {
 
+	private String carpoolId;
+	private String firstName;
+	private String lastName;
+	private String contactNumber;
+	private String vehicleNumber;
+	private Integer noOfSeatsAvailable;
+	private String source;
+	private String destination;
+
+	public String getCarpoolId() {
+		return carpoolId;
+	}
+
+	public void setCarpoolId(String carpoolId) {
+		this.carpoolId = carpoolId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getContactNumber() {
+		return contactNumber;
+	}
+
+	public void setContactNumber(String contactNumber) {
+		this.contactNumber = contactNumber;
+	}
+
+	public String getVehicleNumber() {
+		return vehicleNumber;
+	}
+
+	public void setVehicleNumber(String vehicleNumber) {
+		this.vehicleNumber = vehicleNumber;
+	}
+
+	public String getSource() {
+		return source;
+	}
+
+	public void setSource(String source) {
+		this.source = source;
+	}
+
+	public String getDestination() {
+		return destination;
+	}
+
+	public void setDestination(String destination) {
+		this.destination = destination;
+	}
+
+	public Integer getNoOfSeatsAvailable() {
+		return noOfSeatsAvailable;
+	}
+
+	public void setNoOfSeatsAvailable(Integer noOfSeatsAvailable) {
+		this.noOfSeatsAvailable = noOfSeatsAvailable;
+	}
 }
